e717c4a7fee013936c5f8941adc4de5ba13b743c
Jason Winget <jwinget@gmail.com>
2011-10-24 00:30:57 +0000

    Lots of flailing about with sorting multi-protein queries

--- carpy.py
+++ carpy.py
@@ -3,6 +3,7 @@ from mongokit import Connection, Document
 import uuid
 import os
 import subprocess
+import csv
 
 #---- Reverse Proxy Fix ----#
 class ReverseProxied(object):
@@ -61,6 +62,107 @@ def protein_query(i):
 		return sgid_query
 
 #---- Misc ----#
+name_dict = {
+'genename':'Gene Name',
+'sgid':'SGID',
+'molecularweight':'Molecular Weight',
+'pi':'Isoelectric Point',
+'cai':'Codon Adaptation Index',
+'proteinlength':'Protein Length',
+'codonbias':'Codon Bias',
+'ala':'Alanines',
+'arg':'Arginines',
+'asn':'Asparagines',
+'asp':'Aspartic Acids',
+'cys':'Cysteines',
+'gln':'Glutamines',
+'glu':'Glutamic Acids',
+'gly':'Glycines',
+'his':'Histidines',
+'ile':'Isoleucines',
+'leu':'Leucines',
+'lys':'Lysines',
+'met':'Metionines',
+'phe':'Phenylalanines',
+'pro':'Prolines',
+'ser':'Serines',
+'thr':'Threonines',
+'trp':'Tryptophans',
+'tyr':'Tyrosines',
+'val':'Valines',
+'fopscore':'Freq of Optimal Codons Score',
+'gravyscore':'GRAVY Score',
+'aromaticityscore':'Aromaticity Score',
+'genetype':'Gene Type',
+'orfid':'ORF ID',
+'abundance':'Protein Abundance',
+'localization':'Localization',
+'ambiguous':'Ambiguous Localization',
+'mitochondrion':'In Mitochondria',
+'vacuole':'In Vacuole',
+'spindlepole':'At Spindle Pole',
+'cellperiphery':'At Cell Periphery',
+'punctatecomposite':'At Punctate Composite',
+'vacuolarmembrane':'In Vacuolar Membrane',
+'er':'In Endoplasmic Reticulum',
+'nuclearperiphery':'At Nuclear Periphery',
+'endosome':'In Endosome',
+'budneck':'At Bud Neck',
+'microtubule':'Near Microtubules',
+'golgi':'In Golgi',
+'lategolgi':'In Late Golgi',
+'peroxisome':'In Peroxisome',
+'actin':'Near Actin',
+'nucleolus':'In Nucleolus',
+'cytoplasm':'In Cytoplasm',
+'ertogolgi':'In ER to Golgi Transition',
+'earlygolgi':'In Early Golgi',
+'lipidparticle':'In Lipid Particle',
+'nucleus':'In Nucleus',
+'bud':'At Bud',
+'interactorsms':'Interactors by Mass Spec',
+'interactorsyeast':'Interactors by Yeast Two-Hybrid',
+'disorder':'Percent Disorder (DISOPRED2)',
+'essential':'Essential Gene',
+'halflife':'Protein Half-Life',
+'absolutecost':'Absolute Cost',
+'evolutionaryrate':'Evolutionary Rate',
+'multifunctional':'Rank of Multifunctionality',
+'percentala':'% Alanine',
+'percentarg':'% Arginine',
+'percentasn':'% Asparagine',
+'percentasp':'% Aspartic Acid',
+'percentcys':'% Cysteine',
+'percentgln':'% Glutamine',
+'percentglu':'% Glutamic Acid',
+'percentgly':'% Glycine',
+'percenthis':'% Histidine',
+'percentile':'% Isoleucine',
+'percentleu':'% Leucine',
+'percentlys':'% Lysine',
+'percentmet':'% Methionine',
+'percentphe':'% Phenylalanine',
+'percentpro':'% Proline',
+'percentser':'% Serine',
+'percentthr':'% Threonine',
+'percenttrp':'% Tryptophan',
+'percenttyr':'% Tyrosine',
+'percentval':'% Valine',
+'weissmanprion':'Prion-Like (Weissman)',
+'tango':'Aggregation-Prone (TANGO)',
+'gersteinprion':'Prion-Like (Gerstein)',
+'lindquistprion':'Prion-Like (Lindquist)',
+'iupredlong':'% Long-Stretch Disorder (IUPRED)',
+'iupredshort':'% Short-Stretch Disorder (IUPRED)',
+'disobindinglong':'% Long Disordered Binding (ANCHOR)',
+'disobindingshort':'% Short Disordered Binding (ANCHOR)',
+'percentiupredlongbinding':'% Long Disorder in Binding (ANCHOR)',
+'percentiupredshortbinding':'% Short Disorder in Binding (ANCHOR)',
+'scansitemotifs':'Scansite Motifs',
+'percentscansitemotifs':'Scansite Motifs/Length',
+'hubtype':'Protein Hub Type',
+'openbiosystem.tap':'Open Biosystems TAP Plate Location'
+}
 
 #---- Views ----#
 @app.route('/')
@@ -112,15 +214,44 @@ def query(d):
 	query_dir = 'queries/'+d+'/'
 	f = open(query_dir+'ids.txt', 'r')
 	ids = f.read().splitlines()
-	# Run the R script to determine enriched properties
-	outfile = query_dir+'enrichment_test.out'
-	subprocess.call(['Rscript', 'scripts/enrichment_test.R', query_dir])
+	
+	# Collect unidentified ids
 	n = open('queries/'+d+'/not_found.txt', 'r')
 	not_found = n.read().splitlines()
 	n.close()
+
+	# Run the R script to determine enriched properties
+	outfile = query_dir+'enrichment_test.out'
+	subprocess.call(['Rscript', 'scripts/enrichment_test.R', query_dir])
+
+	# Process enrichment data
+	enriched_dict = {}
+	deenriched_dict = {}
+	qe = open(query_dir+'quant_enrichment.csv', 'r')
+	quant_enrichment = csv.reader(qe)
+	for row in quant_enrichment:
+		if row[0] != '':
+			if float(row[1]) < 0.05:
+				if float(row[2]) > 0:
+					enriched_dict[row[0]] = [row[1], row[2]]
+				else:
+					deenriched_dict[row[0]] = [row[1], row[2]]
+	qe.close()
+	ce = open(query_dir+'cat_enrichment.csv', 'r')
+	cat_enrichment = csv.reader(ce)
+	for row in cat_enrichment:
+		if row[0] != '':
+			if float(row[1]) < 0.05:
+				if float(row[2]) > 1:
+					enriched_dict[row[0]] = [float(row[1]), float(row[2])]
+				else:
+					deenriched_dict[row[0]] = [float(row[1]), float(row[2])]
+	ce.close()
+
+	# Display proteins
 	result = [] 
 	for i in ids:
 		q = protein_query(i)
 		result.append(q)
-	return render_template('retrieve.html', not_found=not_found, result=result)
+	return render_template('retrieve.html', not_found=not_found, result=result, enriched=enriched_dict, deenriched=deenriched_dict, name_dict=name_dict)
 	f.close()
